6224271ecd70cf95883dc76065ce20b5b0f8d168
artmadeit <this.arthur.email@gmail.com>
2024-10-22 00:13:57 -0500

    feat: upload file and show sample

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+st.title("Modelo para pandemia COVID 19")
+
+with st.sidebar:
+    st.write("Requirimientos de datos")
+    st.caption("Para inferir el modelo, debe cargar un archivo en formato csv con las siguientes columnas")
+    with st.expander("Formato de datos"):
+      st.markdown("- Deaths (Muertos)")
+      st.markdown("- Recovered (Recuperados)")
+      st.markdown("- Tested (Probados)")
+      st.markdown("- NewAdded (Agregados recientemente)")
+      # agregar resto de campos
+
+uploaded_file = st.file_uploader("Selecciona un archivo", type="csv")
+if uploaded_file is not None:
+  dataframe = pd.read_csv(uploaded_file)
+  st.write("Una muestra de lo subido:")
+  st.write(dataframe.sample(5))
+  st.write(f"Número de filas: {len(dataframe)}")
