acb00c635192292410573adaf6e457e99bf73089
artmadeit <this.arthur.email@gmail.com>
2024-10-22 00:32:34 -0500

    feat: download csv

--- app.py
+++ app.py
@@ -19,3 +19,17 @@ if uploaded_file is not None:
   st.write("Una muestra de lo subido:")
   st.write(dataframe.sample(5))
   st.write(f"Número de filas: {len(dataframe)}")
+  
+
+  # model = joblib.load('model.joblib')
+  # pred = model.predict_proba(df)
+  # pred = pd.DataFrame(pred, columns = ['setosa_probability', 'versicolor_probability', 'virginica_probability'])
+  pred = dataframe # TODO: delete this
+  st.header('Predicted values')
+  st.write(pred.head())
+  pred = pred.to_csv(index=False).encode('utf-8')
+  st.download_button('Download prediction',
+                  pred,
+                  'prediction.csv',
+                  'text/csv',
+                  key='download-csv')
