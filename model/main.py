94d474defc96bd870cd0c07dadcbcb0ccf3f0366
artmadeit <this.arthur.email@gmail.com>
2024-10-31 15:06:43 -0500

    feat: idea new full_pipeline

--- model/main.py
+++ model/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+
 df1_positivos = pd.read_csv("/content/drive/MyDrive/Desarrollo/DataPorPais/daily_cases_ksa_covid19ArabiaSaudita.csv", sep=',')
 
 def corregir_tested_incorrecto(v):
@@ -19,17 +22,29 @@ df = df1_positivos[['RegionName_EN','Confirmed','Deaths','Recovered','Tested','N
 numeric_cols = df.select_dtypes(include=["float64", "int64"]).columns
 categorical_cols = df.select_dtypes(include=["object"]).columns
 
-scaler = StandardScaler()
-df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+num_pipeline = make_pipeline(StandardScaler())
 
-imputer = SimpleImputer(strategy="most_frequent")
-df[categorical_cols] = imputer.fit_transform(df[categorical_cols])
+cat_pipeline = make_pipeline(SimpleImputer(strategy="most_frequent"), OneHotEncoder())
 
-encoder = OneHotEncoder()
+full_pipeline = ColumnTransformer([
+  ('num', num_pipeline, numeric_cols),
+  ('cat', cat_pipeline, categorical_cols)
+])
 
-encoded_cols = encoder.fit_transform(df[categorical_cols])
-feature_names = encoder.get_feature_names_out(categorical_cols)
-encoded_df = pd.DataFrame(encoded_cols.toarray(), columns=feature_names)
-df_encoded = pd.concat([df.drop(categorical_cols, axis=1), encoded_df], axis=1)
+# df_encoded = full_pipeline.fit_transform()
 
-df_encoded.to_csv("/content/drive/MyDrive/Tesis_Desarrollo/DataPorPais/daily_cases_ksa_covid19ArabiaSauditaDepuradoEncoded.csv", sep=",")
+
+# scaler = StandardScaler()
+# df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+# imputer = SimpleImputer(strategy="most_frequent")
+# df[categorical_cols] = imputer.fit_transform(df[categorical_cols])
+# encoder = OneHotEncoder()
+# encoded_cols = encoder.fit_transform(df[categorical_cols])
+
+#region TODO
+# feature_names = encoder.get_feature_names_out(categorical_cols)
+# encoded_df = pd.DataFrame(encoded_cols.toarray(), columns=feature_names)
+# df_encoded = pd.concat([df.drop(categorical_cols, axis=1), encoded_df], axis=1)
+#endregion 
+
+# df_encoded.to_csv("/content/drive/MyDrive/Tesis_Desarrollo/DataPorPais/daily_cases_ksa_covid19ArabiaSauditaDepuradoEncoded.csv", sep=",")
